637fcdea6cf5a9be96fcb7ae24f5c297ee6a40b4
Doniyorov-Aziz <azizdoniyorov272@gmail.com>
2023-06-06 17:16:36 +0500

    Js|Variables|Data types

--- Lesson_001/index.js
+++ Lesson_001/index.js
@@ -3,4 +3,37 @@
 // console.log(`hello ${"name"}`); // hello name
 // console.log(`hello ${name}`); // hello Ilya
 
-// console.log();
+// let user = {
+//   id: 1,
+//   name: "Lena Graham",
+//   surname: "Bret",
+//   email: "Sincere@april.biz",
+//   address: {
+//     street: "Kulas light",
+//     suite: "Apl",
+//     city: "Grenbrouht",
+//     zipcode: "92998-283",
+//     geo: {
+//       lat: "-212.32",
+//       lng: "2153.32",
+//     },
+//   },
+//   phone: "12-312x-221453x",
+//   website: "hildegard.org",
+//   company: {
+//     name: "Romagure",
+//     catchprise: "Multi",
+//     bs: "harness",
+//   },
+// };
+// console.log(`User info
+// ${user.id}
+// ${user.name}
+// ${user.address.street}
+// ${user.address.geo.lng}`);
+
+// let name = "Jack";
+// let year = "2013";
+// let status = "Softwear Enginer";
+
+// console.log(`${name} is a ${2023 - year} year old ${status}`);
