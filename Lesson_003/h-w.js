830cf97690b87195dd9ce507139791138be8fbf4
Doniyorov-Aziz <azizdoniyorov272@gmail.com>
2023-06-08 14:07:34 +0500

    If else | Nullish

---
+++ Lesson_003/h-w.js
@@ -0,0 +1,39 @@
+// let a = 1,
+//   b = 1;
+// let c = a++;
+// let d = ++b;
+// console.log(c); // 1
+// console.log(d); // 2
+
+//====================
+
+// let a = 2;
+
+// let x = 1 + (a *= 2);
+// console.log(x);  // 5
+
+//====================
+
+// console.log("" + 1 + 0); // 10
+// console.log("" - 1 + 0); // -1
+// console.log(true + false); // 1
+// console.log(6 / "3"); // 2
+// console.log(4+5+"px"); // 9px
+// console.log("$"+4+5); // $45
+// console.log("4"-2); // 2
+// console.log("4px" - 2); // NaN
+// console.log("-9" + 5); // -95
+// console.log("-9" - 5); // -14
+// console.log(null + 1); // 1
+// console.log(undefined + 1); // NaN
+// console.log("\t\n"-2); // -2
+
+//=====================
+
+// console.log(5 > 4); // true
+// console.log("apple" > "penapple"); // false
+// console.log("2" > "12"); // true
+// console.log(null == undefined); // true
+// console.log(undefined === null); // false
+// console.log("null" == "\n0\n"); // false
+// console.log(null === +"n\0\n"); // false
