fc83f2dd8bbb13edad93fd41233d597426cb44a0
Doniyorov-Aziz <azizdoniyorov272@gmail.com>
2023-06-08 12:20:18 +0500

    If else | Nullish

---
+++ Lesson_003/index.js
@@ -0,0 +1,85 @@
+// If else | Nullish
+// true = 1
+// false = 0 ; null ; undefined ; "" ; false ; NaN
+
+// let temp = 1;
+
+// if (temp) {
+//   console.log("havo sovuq");
+// }
+// if (true) {
+//   console.log("havo issiq");
+// }
+
+//=============================
+
+// let temp = "yomgir"
+
+// if(temp === "sovuq"){
+//     console.log("havo sovuq,qalin kiyinb oling");
+// }else if(temp === "yomgir"){
+//     console.log("havo yomgir,soyobon oling");
+// }else if (temp === "issiq"){
+//     console.log("havo issiq , yengl kiyinb oling");
+// }else if(temp==="qor"){
+//     console.log("havo qor, uydan chiqmang");
+// }else{
+//     console.log("xato malumot kiritildi");
+// }
+
+//=============================
+
+// let temp = 10;
+
+// if (temp < 0) {
+//   console.log("havo juda sovuq");
+// } else if (temp <= 10) {
+//   console.log("havo sovuq");
+// } else if (temp < 15) {
+//   console.log("havo issiq");
+// } else if (temp < 25) {
+//   console.log("havo juda issiq");
+// }
+
+//=============================
+
+// let temp = 23;
+// if (temp > 0 && temp <= 10) {
+//   console.log("havo sovuq");
+// } else if (temp > 10 && temp <= 20) {
+//   console.log("havo issiq yengil kiyinb oling");
+// } else if (temp > 20 && temp <= 30) {
+//   console.log("havo juda issiq soyabo oling");
+// } else if (temp > 30) {
+//   console.log("havo juda issiq uyda otiring");
+// }
+
+//=============================
+
+//Turnary operator
+// if = ?   else = :
+
+// let temp = 20;
+
+// temp < 0 ? console.log("havo sovuq") : console.log("not found");
+
+// let temp = 2;
+// temp > 0 && temp <= 10
+//   ? console.log("havo sovuq")
+//   : temp > 10 && temp <= 20
+//   ? console.log("havo issiq yengil kiyinb oling")
+//   : temp > 20 && temp <= 30
+//   ? console.log("havo juda issiq soyabon oling")
+//   : temp > 30
+//   ? console.log("havo juda issiq uyda otiring")
+//   : console.log("not found");
+
+//=============================
+
+// Nullish ?? --> null , undefined
+
+// console.log(null ?? undefined); // undefined
+// console.log(undefined ?? null); // null
+// console.log(2 ?? undefined); // 2
+// console.log(null ?? 1); // 1
+// console.log(1 ?? 2); // 1
