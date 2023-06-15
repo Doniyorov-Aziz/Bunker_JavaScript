684315b542e3bf11c081bedccee4ca340a21def2
Doniyorov-Aziz <azizdoniyorov272@gmail.com>
2023-06-15 11:35:48 +0500

    Switch-case | While | Do while | For

---
+++ Lesson_004/index.js
@@ -0,0 +1,111 @@
+// Switch case
+
+// let chiroq = "sariq";
+// switch (chiroq) {
+//   case "qizil":
+//     console.log(`to'xta`);
+//     break;
+//   case "sariq":
+//     console.log("tayyorlan");
+//     break;
+//   case "yashil":
+//     console.log("yur");
+//     break;
+//   default:
+//     console.log("xato");
+// }
+
+//=============================
+
+// let browser = "Safari";
+// switch (browser) {
+//   case "Edge":
+//     console.log("You have got the Edge");
+//     break;
+//   case "Chrome":
+//   case "Firefox":
+//   case "Safari":
+//   case "Opera":
+//     console.log("Okey we support these browsers too");
+//     break;
+//   default:
+//     console.log("We hope that this page looks ok !");
+// }
+
+// let browser = "Edge";
+// if (browser === "Edge") console.log("You have got the Edge");
+// else if (
+//   browser === "Chrome" ||
+//   browser === "Firefox" ||
+//   browser === "Safari" ||
+//   browser === "Opera"
+// ) {
+//   console.log("Okey we support these browsers too");
+// } else console.log("We hope that this page looks ok !");
+
+//==============================
+
+// While
+
+// 1 dan 100 gacha bo'lgan sonlarni chiqarish
+
+// let i = 1;
+// while (i <= 100) {
+//   console.log(i);
+//   i += 1;
+// }
+
+// let i = 1;
+// while (i <= 100) {
+//   console.log(i);
+//   i += 1;
+// }
+// console.log(i);  // 101
+
+//==============================
+
+// juft va toq sonlarni chiqarish
+
+// let i = 1;
+// while (i <= 100) {
+//   if (i % 2 === 0) console.log(`${i} - juft son`);
+//   i += 1;
+// }
+
+// let i =1
+// while(i<=100){
+//     if(i%2===1)console.log(`${i} - toq son`);
+//     i += 1
+// }
+
+// let i = 1;
+// while (i <= 100) {
+//   if (i % 2 === 1) console.log(`${i} - toq son`);
+//   else if (i % 2 === 0) console.log(`${i} - juft son`);
+//   i += 1;
+// }
+
+//==============================
+
+// do while
+
+// let i = 1;
+// do {
+//   console.log(i);
+//   i += 1;
+// } while (i <= 10);
+
+// let i = 1;
+// do {
+//   console.log(i);
+//   i += 1;
+// } while (i < 1); // 1
+// console.log(i,"tashqarida"); // 2
+
+//==============================
+
+// for 
+// for(let i = 1 ; condition ; i++)
+
+// toq son topish
+
