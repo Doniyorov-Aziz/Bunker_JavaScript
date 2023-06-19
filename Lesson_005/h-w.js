7ac763016f9bc636afac31cd9e1790a1291f7b15
Doniyorov-Aziz <azizdoniyorov272@gmail.com>
2023-06-19 12:59:46 +0500

    Function

---
+++ Lesson_005/h-w.js
@@ -0,0 +1,23 @@
+// 1
+
+// const getNumPalindrom = (num) => {
+//   let res = "";
+//   for (let i = num.length - 1; i >= 0; i--) {
+//     res += num[i];
+//   }
+//   return res;
+// };
+// console.log(getNumPalindrom("1232"));
+
+//==========================
+
+// 2
+
+// const getNumPalindrom=(num)=>{
+//     let res="";
+//     for (let i = num.length-1; i >=0 ; i--) {
+//         res+=num[i];
+//     }
+//     return res==num?`Plindrom`:"No palindrom"
+//  }
+// console.log(getNumPalindrom("121"));
