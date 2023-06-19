883c9ada8cd0d0f3675325fce123e9c7e0052e74
Doniyorov-Aziz <azizdoniyorov272@gmail.com>
2023-06-19 10:24:49 +0500

    Function

--- Lesson_005/index.js
+++ Lesson_005/index.js
@@ -1,1 +1,43 @@
 // function
+//function decloration
+// dinamc - bir martta functionni chaqirish
+// reusable - bir necha martta functionni chaqirish
+
+// 1
+
+// function sayHello() {
+//   console.log("Hello");
+// }
+// sayHello();
+
+// 2
+
+// function sayHello(fullname) {
+//   console.log(`Hello ${fullname}`);
+// }
+
+// sayHello("Azizbek");
+// sayHello("webbrian");
+// sayHello("Ashrafxon");
+
+//======================
+
+// function expression
+
+// 1
+
+// const sayHello = function () {
+//     console.log("Hello");
+// };
+
+// sayHello()
+
+// 2
+
+// const sayHello = function (fullname) {
+//   console.log(`Hello ${fullname}`);
+// };
+// sayHello("Webbrain");
+// sayHello("Digital");
+
+
