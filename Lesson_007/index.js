6f2902d90b68d6734bb4201d37574203280d4b6f
Doniyorov-Aziz <azizdoniyorov272@gmail.com>
2023-06-21 12:19:05 +0500

    Advanced Object | Objects - 2

---
+++ Lesson_007/index.js
@@ -0,0 +1,40 @@
+// let obj = {
+//   name: "webbrian",
+//   age: 23,
+//   child: {
+//     title: "It center",
+//     age: 12,
+//     child: {
+//       major: "Frontend",
+//       age: 33,
+//     },
+//   },
+// };
+
+// let sum = 0;
+
+// while (1) {
+//   if (obj) sum += obj.age;
+//   else break;
+//   obj = obj.child;
+// }
+// console.log(sum);
+
+//=====================
+
+// function sayHi() {
+//   console.log("Hi");
+// }
+// let name = "webbrain";
+
+// let obj = {
+//   name,
+//   sayHi() {
+//     console.log("I said Hi");
+//   },
+// };
+// obj.sayHi();
+// console.log(obj.name);
+
+//=====================
+//log
