e7e9afcc9f0cae4c966178e937ac25d80cb6c48d
Doniyorov-Aziz <azizdoniyorov272@gmail.com>
2023-06-23 17:29:56 +0500

    Advanced Object | Objects - 2

--- Lesson_007/index.js
+++ Lesson_007/index.js
@@ -37,4 +37,116 @@
 // console.log(obj.name);
 
 //=====================
-//log
+// shallow copy - bu ustidan copiy qilish
+
+// let obj = { name: "web", info: { title: 2 } };
+// let copy1 = Object.assign({}, obj);
+// copy1.name = "webbrain";
+// copy1.info.title = 3;
+// console.log(copy1, "shallow copy");
+// console.log(obj, "real copy");
+
+//=====================
+// deep copy - bu har tomonlama copy qilish
+
+// let obj = { name: "web", info: { title: 2 } };
+// let copy2 = structuredClone(obj);
+
+// copy2.name = "webbrain";
+// copy2.info.title = 3;
+// console.log(copy2, "deep copy");
+// console.log(obj, "real copy");
+
+//=====================
+// Object in object
+
+// let grandFather = {
+//   name: "aziz",
+//   age: 21,
+//   child: {
+//     name: "laziz",
+//     age: 18,
+//     child: {
+//       name: "ozod",
+//       age: 12,
+//     },
+//   },
+// };
+
+// let getSum = (obj) => {
+//   let result = 0;
+//   while (obj) {
+//     result += obj.age;
+//     obj = obj.child;
+//   }
+//   return result;
+// };
+// console.log(getSum(grandFather));
+
+//=====================
+
+// let obj1 = { name: "webbrain" };
+// let obj2 = obj1;
+
+// obj1.name = "academy";
+// console.log(obj2);
+
+//=====================
+// garbage collection
+
+// let obj = { name: "webbrian" };
+// obj = { title: "academy" };
+
+// obj = "webbrian";
+// obj = 1234;
+
+//1
+
+// const obj = {
+//   name: "wba",
+//   sayHi: function () {
+//     console.log("Hi");
+//   },
+// };
+// obj.sayHi()
+
+//2
+
+// const obj = {
+//   name: "wba",
+//   sayHi: () => {
+//     return "Hi";
+//   },
+// };
+// console.log(obj.sayHi());
+
+//3
+
+// const obj = {
+//   name: "wba",
+//   sayHi() {
+//     console.log("Hi");
+//   },
+// };
+// obj.sayHi();
+
+//4
+
+// function sayHi() {
+//   console.log("Hi");
+// }
+// const obj = {
+//   name: "wba",
+//   ourFunction: sayHi,
+// };
+// obj.ourFunction();
+
+// this
+
+// let obj = {
+//   name: "webbrain",
+//   sayHi() {
+//     console.log(`Hi ${this.name}`);
+//   },
+// };
+// obj.sayHi();
--- Lesson_007/review.js
+++ Lesson_007/review.js
@@ -1,1 +1,16 @@
 // Advanced Object | Objects - 2
+
+// shallow copy - bu ustidan copiy qilish
+// object.Assign() bu shallow copy yasashga yordam beradi
+
+// shallow copy bu sayoz copy yani ustidan copy qilib oladi ichiga kiradigan bo'lsa reference ni ham oladi
+
+// deep copy - bu har tomonlama copy qilish
+// structuredClone bu bizga deep copy yasab beradi bunda deep copyda object qanaqa bulsa shuni ozide copy qiladi
+// shallow copy esa referencesini copy qilib oladi
+
+// garbage collection - bu objectdan stringga , numberga o'tgan vaqti bizga boshqa kerak bo'lmaydigan qiymatlar garbage collectionga tushib qoladi
+// objectga boradigan link o'chib ketsa yoki yangilansa oldingi qiymatlar garbage collectionga yani korzinkaga tushadi
+
+// this o'zi turgan scope ni yani gulli qavs ni {} qaytaradi
+// this = window
